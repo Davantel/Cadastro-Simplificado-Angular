fe00be20a618c443f660c32d78ef0508af6e2ca6
Caio Vinicius Davantel <caiodavantel@rede.ulbra.br>
2020-03-12 21:47:24 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'cadastroSP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cadastroSP');
+    expect(app.titulo).toEqual('cadastroSP');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cadastroSP';
+  titulo = '';
+  pessoas = [
+  ];
+
+  cadastro() {
+    this.pessoas.push(this.titulo);
+    this.titulo = null;
+  }
 }
+
+
